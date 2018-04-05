3889e873dca4f5db548c46c5a7323539f1e0f890
Nicolas Pochet <npochet@gmail.com>
2018-04-05 20:12:27 +0200

    Remove target

--- ch2/hello/target/debug/deps/hello-9cf1d732b24de27d.d
+++
@@ -1,5 +0,0 @@
-/home/npochet/programmer-exercises/ch2/hello/target/debug/deps/hello-9cf1d732b24de27d: src/main.rs
-
-/home/npochet/programmer-exercises/ch2/hello/target/debug/deps/hello-9cf1d732b24de27d.d: src/main.rs
-
-src/main.rs:
--- ch2/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/home/npochet/programmer-exercises/ch2/hello/target/debug/hello: /home/npochet/programmer-exercises/ch2/hello/src/main.rs
