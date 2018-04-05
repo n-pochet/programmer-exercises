38e28eb8daf79cac4b118edfb453ea2fec36b939
Nicolas Pochet <npochet@gmail.com>
2018-04-05 20:24:29 +0200

    Rust fmt

--- ch2/char_count/src/main.rs
+++ ch2/char_count/src/main.rs
@@ -3,7 +3,9 @@ use std::io;
 fn main() {
     let mut buffer = String::new();
     println!("What is the input string?");
-    io::stdin().read_line(&mut buffer).expect("Couldn't read from stdin");
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Couldn't read from stdin");
     let trimmed_input = buffer.trim();
     let count = trimmed_input.len();
     println!("{} has {} characters.", trimmed_input, count);
--- ch2/hello/src/main.rs
+++ ch2/hello/src/main.rs
@@ -3,7 +3,9 @@ use std::io;
 fn main() {
     let mut name = String::new();
     println!("What is your name?");
-    io::stdin().read_line(&mut name).expect("Couldn't read from stdin");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Couldn't read from stdin");
     let name = name.trim();
     println!("Hello, {}, nice to meet you!", name);
 }
