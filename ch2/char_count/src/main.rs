19558be58760ffb8411cd17b0f926bffc124bf00
Nicolas Pochet <npochet@gmail.com>
2018-04-05 20:23:06 +0200

    Add char_count

---
+++ ch2/char_count/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    println!("What is the input string?");
+    io::stdin().read_line(&mut buffer).expect("Couldn't read from stdin");
+    let trimmed_input = buffer.trim();
+    let count = trimmed_input.len();
+    println!("{} has {} characters.", trimmed_input, count);
+}
